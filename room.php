bdd1b40de64b3821cecb6a77aacb7a7c31b7ee66
Max-KS <124424451+Max-KS@users.noreply.github.com>
2023-02-18 13:52:54 +0700

    Update 2

--- room.php
+++ room.php
@@ -3,7 +3,7 @@
         <tr>
             <td width="23" rowspan="2">&nbsp;</td>
             <td height="119" colspan="2" bgcolor="#006600">
-                <h1><center><font color="#FFFFFF">ประวัติส่วนตัว</font></center></h1>
+                <h1><center><font color="#FFFFFF">ประวัติส่วนตัว แม็ก</font></center></h1>
                 <?php include "Date.php"; ?>
             </td>
             <td width="43" rowspan="2">&nbsp;</td>
@@ -13,7 +13,7 @@
                 <?php include "show_room.php"; ?>
             </td>
             <td width="190" bgcolor="#FFFF66"><ul>
-				<li><a href="home.php">ชื่อ ........</li>
+				<li><a href="home.php">ชื่อ กฤษฎา สินไชย</li>
 				<li><a href="room.php">ที่อยู่ .......</li>
 				<li><a herf="guest.php">เบอร์โทร ........</li>
 				<li><a href="contact.php">อีเมลล์ .......</li>
